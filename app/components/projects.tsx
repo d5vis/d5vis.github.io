aa18b2abcf321a396892035ded5bf5b967240d33
d5vis <davisjbanks@gmail.com>
2024-12-31 13:05:20 -0800

    add project descriptions

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -9,18 +9,21 @@ const Projects = () => {
       <div className="flex flex-col w-full h-full gap-4">
         <Project
           title="Google Vids Playback UI"
+          description="Fullstack, UI/UX (Intern, Summer 2024)"
           link="https://docs.google.com/videos/u/0/"
           image="/images/vids.gif"
           logo="/images/google.png"
         />
         <Project
           title="Android Auto Partner Tools"
+          description="Fullstack (Intern, Summer 2023)"
           link="https://source.android.com/docs/automotive"
           image="/images/taco.jpg"
           logo="/images/android.png"
         />
         <Project
           title="LMUCS TA Site"
+          description="Computer Science TA Schedule & Resources"
           link="https://ta.lmucs.org"
           image="/images/talmucs.png"
           logo="/images/lmu.png"
@@ -29,12 +32,14 @@ const Projects = () => {
       <div className="flex flex-col w-full h-full gap-4">
         <Project
           title="Who's on Duty?"
+          description="RA Duty Schedule for LMU Student Housing"
           link="https://whosonduty.vercel.app/l56"
           image="/images/whosonduty.png"
           logo="/images/lmu.png"
         />
         <Project
-          title="Causal Discovery & Behavior"
+          title="Causal Discovery"
+          description="Research on Causal Inference & Understanding Behavior"
           link="https://drive.google.com/file/d/1wZTY-9hhW0jScWyRyxMbc1HGq27RsKdH/view?usp=sharing"
           image="/images/causal.png"
           logo="/images/lmu.png"

7b0349ecc914ff1a12c62dad949bad4f2309cbd4
d5vis <davisjbanks@gmail.com>
2024-12-31 15:11:05 -0800

    add 404

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-3xl text-gray-800">
+      <h1>
+        <b>404 :(</b>
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;

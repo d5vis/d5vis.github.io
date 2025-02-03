39cd9d3d2fe35f302feb41573aa34d1d8feb6870
d5vis <davisjbanks@gmail.com>
2025-02-02 19:50:19 -0800

    modify spotify embed for mobile

--- app/components/playlist.tsx
+++ app/components/playlist.tsx
@@ -2,7 +2,7 @@ const Playlist = () => {
   return (
     <div className="intersect-once motion-duration-1500 intersect:motion-translate-y-in-[20%] intersect:motion-blur-in relative group flex flex-col items-center justify-center w-full h-full bg-gray-100 text-gray-600 p-6 gap-8 rounded-3xl hover:scale-[101%] hover:shadow-sm transition-all">
       <iframe
-        className="z-20 rounded-xl bg-gray-300"
+        className="z-20 rounded-xl bg-gray-300 min-h-[400px]"
         src="https://open.spotify.com/embed/playlist/58bwbCuxsfTTbfI9gd6kK0?utm_source=generator"
         width="100%"
         height="100%"

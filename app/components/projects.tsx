4cad588d97f2feae0cf0dbedaed8d81312f5a99c
d5vis <davisjbanks@gmail.com>
2025-03-04 17:42:12 -0800

    update projects

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -16,13 +16,6 @@ const Projects = () => {
           logo="/images/google.png"
         />
         <Project
-          title="Android Auto Partner Tools"
-          description="Fullstack (Intern, Summer 2023)"
-          link="https://source.android.com/docs/automotive"
-          image="/images/taco.jpg"
-          logo="/images/android.png"
-        />
-        <Project
           title="NSBE at LMU"
           description="Co-President, National Society of Black Engineers at Loyola Marymount University"
           link="https://nsbelmu.org/"
@@ -30,14 +23,28 @@ const Projects = () => {
           logo="/images/lmu.png"
         />
         <Project
+          title="Android Auto Partner Tools"
+          description="Fullstack (Intern, Summer 2023)"
+          link="https://source.android.com/docs/automotive"
+          image="/images/taco.jpg"
+          logo="/images/android.png"
+        />
+        {/* <Project
           title="Causal Discovery"
           description="Research on Causal Inference & Understanding Behavior"
           link="https://drive.google.com/file/d/1wZTY-9hhW0jScWyRyxMbc1HGq27RsKdH/view?usp=sharing"
           image="/images/causal.png"
           logo="/images/lmu.png"
-        />
+        /> */}
       </div>
       <div className="flex flex-col w-full h-full gap-4">
+        <Project
+          title="Counterfactual Methods & Buyer Retention"
+          description="Machine Learning Research Engineer (Spring 2025)"
+          link="https://ebay.com"
+          image="/images/ebay.png"
+          logo="/images/ebay_logo.png"
+        />
         <Project
           title="LMUCS TA Site"
           description="Computer Science TA Schedule & Resources"
@@ -45,13 +52,13 @@ const Projects = () => {
           image="/images/talmucs.png"
           logo="/images/lmu.png"
         />
-        <Project
+        {/* <Project
           title="Who's on Duty?"
           description="RA Duty Schedule for LMU Student Housing"
           link="https://whosonduty.vercel.app/l56"
           image="/images/whosonduty.png"
           logo="/images/lmu.png"
-        />
+        /> */}
         <Playlist />
       </div>
     </div>

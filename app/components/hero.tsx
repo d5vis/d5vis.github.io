c41e4ac20c9d03bc411f3b0a66556b8cb55b31c9
d5vis <davisjbanks@gmail.com>
2025-03-04 20:11:55 -0800

    add map tooltip

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import MapTooltip from "./map-tooltip";
 
 const Hero = () => {
   return (
@@ -46,43 +47,55 @@ const Hero = () => {
       </nav>
       <div className="flex flex-col items-center justify-center text-white p-8 gap-2 md:flex-row md:gap-8">
         <div className="flex gap-1 motion-blur-in motion-duration-[1s]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-            />
-          </svg>
-          <b>los angeles, california</b>
+          <MapTooltip
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                />
+              </svg>
+            }
+            title="los angeles, california"
+            location="los angeles, california"
+            zoom={9}
+          />
         </div>
         <div className="flex gap-1 motion-blur-in motion-duration-[1s] motion-delay-[0.3s]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-            />
-          </svg>
-          <b>loyola marymount university</b>
+          <MapTooltip
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+                />
+              </svg>
+            }
+            title="loyola marymount university"
+            location="loyola marymount university"
+            zoom={15}
+          />
         </div>
         <div className="flex gap-1 motion-blur-in motion-duration-[1s] motion-delay-[0.6s]">
           <svg
---
+++ app/components/map-tooltip.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface MapTooltipProps {
+  icon: React.ReactNode;
+  title: string;
+  location: string;
+  zoom?: number;
+}
+
+const MapTooltip: React.FC<MapTooltipProps> = ({
+  icon,
+  title,
+  location,
+  zoom = 15,
+}) => {
+  const locationSearchQuery = encodeURIComponent(location);
+
+  return (
+    <div className="relative group w-full flex items-center gap-1">
+      {icon}
+      <b>{title}</b>
+      <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 group-hover:motion-blur-in absolute bottom-full left-1/2 transform -translate-x-1/2 p-4 mb-2 bg-gray-200 text-white text-sm rounded-lg shadow-md pointer-events-none">
+        <iframe
+          tabIndex={-1}
+          className="motion-blur-in motion-delay-300 w-[300px] h-[200px] bg-gray-100 rounded-md aria-hidden"
+          height="600"
+          src={`https://maps.google.com/maps?width=100%25&height=600&hl=en&q=${locationSearchQuery}&t=&z=${zoom}&ie=UTF8&iwloc=B&output=embed`}
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default MapTooltip;
